d4288eebc026b0c9299d34f005acccf5994d7a43
Kishor Nangaliya <116244524+krn9541@users.noreply.github.com>
2025-04-10 07:40:07 +0000

    container config done

---
+++ .devcontainer/basic/variable.js
@@ -0,0 +1,18 @@
+const test = "rtrt"
+let test1 = "1234"
+var test2 = 5678
+test3 = "tyu"
+let test4
+
+//const test = "dwgdf"
+test1 = "gfdewfdf"
+test2 = 6769
+test3 = "ffsafc"
+
+
+
+console.log(test)
+console.table([test,test1,test2,test3,test4]);
+
+    
+
