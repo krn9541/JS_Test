b4fcf903d3f5ee779a332bbdd5dac57c5e7e5b76
Kishor Nangaliya <116244524+krn9541@users.noreply.github.com>
2025-04-10 08:12:25 +0000

    datatype

---
+++ .devcontainer/datatype.js
@@ -0,0 +1,26 @@
+"use strict" ; // treat all js code as newer version
+
+// alert(3 + 3)  //we are using nodejs, not browser
+
+console.log(3+3); 
+console.log("Kishor")
+
+let name = "Karan"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+//bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined);  //undefined
+
